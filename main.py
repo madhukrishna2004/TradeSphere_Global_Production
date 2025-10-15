d8b55a1a495a1be2d043689ea701569d9e989313
madhu_krishna <krishnamadhurama@gmail.com>
2025-10-15 19:02:56 +0530

    Fix: Gunicorn start command for Render

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import os
 from src.app import app
 
 if __name__ == "__main__":
-    # Get the port Render provides, default to 5000 if running locally
     port = int(os.environ.get("PORT", 5000))
-    
-    # Bind to all network interfaces (0.0.0.0) so Render can reach it
     app.run(host="0.0.0.0", port=port)
