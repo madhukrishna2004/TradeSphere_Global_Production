173376af7360941255cf30777f9e933a97066c25
madhu_krishna <krishnamadhurama@gmail.com>
2025-10-16 22:50:29 +0530

    new update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-from src.app import app  # ensure src/__init__.py exists
+from src.app import app 
 
-# ✅ This ensures Gunicorn can see `app` directly
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+
+
+    
